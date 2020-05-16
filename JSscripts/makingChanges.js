d3b301a8a3e1478e1330132180b5a1ceae88aeef
chloe ridley <chloeridley@chloes-MBP.hitronhub.home>
2020-05-16 17:55:54 -0600

    finished the example page for inserting creating and removing nodes

--- JSscripts/makingChanges.js
+++ JSscripts/makingChanges.js
@@ -8,8 +8,17 @@ let toDoList = document.getElementById('toDo');
 
 let newToDo = document.createElement('li');
 
-/*putting a border around example*/
+/*putting a border around example variables*/
 let examples1 = document.getElementsByClassName('examples');
+
+/*replace list variables*/
+let rep = document.getElementById('replaceList');
+
+let rep1 = document.createElement('li');
+
+/*removing node variables*/
+
+let example = document.getElementById('removingNode');
 
 /*Grocery list methods */
 newItem.textContent = 'cheese';
@@ -19,9 +28,21 @@ groceryList.appendChild(newItem);
 /*to-do list methods*/
 newToDo.textContent = 'Write code';
 
-toDoList.insertBefore(newToDo, toDoList.lastElementChild);
+toDoList.insertBefore(newToDo, toDoList.children[1]);
 
 /*putting a border around example method*/
 for (i = 0; i < examples1.length; i++){
     examples1[i].style.border = '5px solid purple';
 }
+
+/*replace list methods*/
+
+rep1.textContent = 'ooops';
+
+rep.replaceChild(rep1, rep.children[6]);
+
+/*removing node methods*/
+
+example.removeChild(example.children[3]);
+
+example.children[0].remove();

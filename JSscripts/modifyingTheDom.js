15963aae3db9788e45bc31584152deea9a97f0c2
chloe ridley <chloeridley@chloes-MBP.hitronhub.home>
2020-05-18 17:27:43 -0600

    finished modifying examples

--- JSscripts/modifyingTheDom.js
+++ JSscripts/modifyingTheDom.js
@@ -1,3 +1,11 @@
 let setEx = document.getElementById('imageEx');
 
 setEx.setAttribute('src', '../images/cartoon-cat-free.png');
+
+let blueEx = document.getElementById('blue');
+
+blueEx.className="red";
+
+let backgroundEx = document.getElementById('red');
+
+backgroundEx.classList.add('background');

78e87124425b64677190f706b0946ac42d4d0b87
chloe ridley <chloeridley@chloes-MBP.hitronhub.home>
2020-06-05 10:26:15 -0600

    finished the working with strings page

---
+++ JSscripts/workingWIthStrings.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $("#functionTrigger").click(function(){
+        $(".func").toggle();
+    });
+
+     $("#joinTrigger").click(function(){
+        $("#join").toggle();
+    });
+});
